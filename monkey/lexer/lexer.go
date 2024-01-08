ace389f9b787d9e3994d9fc7dbfe23c303c53dd3
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2024-01-08 14:45:24 +0530

    basic monkey lexing complete

--- monkey/lexer/lexer.go
+++ monkey/lexer/lexer.go
@@ -6,7 +6,7 @@ import (
 
 type Lexer struct {
 	input        string
-	postition    int  // current position in input (points to current char)
+	position     int  // current position in input (points to current char)
 	readposition int  // current reading position in input (after current char)
 	ch           byte // current char under examination
 }
@@ -27,12 +27,15 @@ func (l *Lexer) readChar() {
 	} else {
 		l.ch = l.input[l.readposition]
 	}
-	l.postition = l.readposition
+	l.position = l.readposition
 	l.readposition += 1
 }
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -53,6 +56,18 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -61,4 +76,38 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+
+	for isLetter(l.ch) {
+		// this works because readchar keeps updating the l.position everytime it is called
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
